44aa9e3dd81de98e70c1207212d4af3a9ad7d800
Eddy <eddy.nguyen@99designs.com>
2019-03-04 10:02:15 +1100

    Add signup form no hook

---
+++ src/common/components/Paper/Paper.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Paper.css';
+
+const Paper: React.FunctionComponent = ({ children }) => (
+  <div className="paper">{children}</div>
+);
+
+export default Paper;
--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,8 +1,60 @@
 import React from 'react';
+import Paper from 'src/common/components/Paper';
+import Row from 'src/common/components/Row';
 
-class Signup extends React.Component<{}> {
+interface State {
+  email: string;
+  password: string;
+}
+
+class Signup extends React.Component<{}, State> {
+  state: State = {
+    email: '',
+    password: ''
+  };
+  setEmail = (email: string): void => {
+    this.setState({ email });
+  }
+  setPassword = (password: string): void => {
+    this.setState({ password });
+  }
   render() {
-    return <div />;
+    const { email, password } = this.state;
+    return (
+      <Paper>
+        <form>
+          <h1>Sign up</h1>
+          <Row>
+            <div>
+              <label>Email</label>
+            </div>
+            <div>
+              <input
+                name="email"
+                value={email}
+                onChange={e => this.setEmail(e.target.value)}
+              />
+            </div>
+          </Row>
+          <Row>
+            <div>
+              <label>Password</label>
+            </div>
+            <div>
+              <input
+                name="password"
+                type="password"
+                value={password}
+                onChange={e => this.setPassword(e.target.value)}
+              />
+            </div>
+          </Row>
+          <Row>
+            <button>Signup</button>
+          </Row>
+        </form>
+      </Paper>
+    );
   }
 }
 
