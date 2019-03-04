8fdce2852a6facf5e2d5b822978d2f829eafb512
Eddy <eddy.nguyen@99designs.com>
2019-03-04 11:05:55 +1100

    Add side-effects to dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -3,17 +3,25 @@ import { Redirect } from 'react-router';
 import Row from 'src/common/components/Row';
 import { ViewerConsumer } from 'src/common/components/ViewerContext/ViewerContext';
 
-const Dashboard: React.FunctionComponent = () => {
-  return (
-    <ViewerConsumer>
-      {({ viewer }) => {
-        if (!viewer) {
-          return <Redirect to="/" />;
-        }
-        return <Row>Welcome {viewer.email}!</Row>;
-      }}
-    </ViewerConsumer>
-  );
-};
+class Dashboard extends React.Component {
+  componentDidMount() {
+    alert('Welcome to dashboard!');
+  }
+  componentWillUnmount() {
+    alert('Goodbye friend!');
+  }
+  render() {
+    return (
+      <ViewerConsumer>
+        {({ viewer }) => {
+          if (!viewer) {
+            return <Redirect to="/" />;
+          }
+          return <Row>Welcome {viewer.email}!</Row>;
+        }}
+      </ViewerConsumer>
+    );
+  }
+}
 
 export default Dashboard;
