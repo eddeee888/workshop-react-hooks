6a815c6dff736a36a1f923eab69c9edbf66de073
Eddy <eddy.nguyen@99designs.com>
2019-03-04 11:01:28 +1100

    Make dashboard a function component

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -3,19 +3,17 @@ import { Redirect } from 'react-router';
 import Row from 'src/common/components/Row';
 import { ViewerConsumer } from 'src/common/components/ViewerContext/ViewerContext';
 
-class Dashboard extends React.Component<{}> {
-  render() {
-    return (
-      <ViewerConsumer>
-        {({ viewer }) => {
-          if (!viewer) {
-            return <Redirect to="/" />;
-          }
-          return <Row>Welcome {viewer.email}!</Row>;
-        }}
-      </ViewerConsumer>
-    );
-  }
-}
+const Dashboard: React.FunctionComponent = () => {
+  return (
+    <ViewerConsumer>
+      {({ viewer }) => {
+        if (!viewer) {
+          return <Redirect to="/" />;
+        }
+        return <Row>Welcome {viewer.email}!</Row>;
+      }}
+    </ViewerConsumer>
+  );
+};
 
 export default Dashboard;
